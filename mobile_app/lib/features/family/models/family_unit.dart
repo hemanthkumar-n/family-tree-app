daabb0208790f96b94b2d704b8a21bf5448e5355
Hemanth Kumar <hemanthpresence@gmail.com>
2026-06-01 00:19:14 +0530

    feat(graph): introduce family unit model

---
+++ mobile_app/lib/features/family/models/family_unit.dart
@@ -0,0 +1,11 @@
+class FamilyUnit {
+  final String id;
+  final String partner1;
+  final String partner2;
+
+  const FamilyUnit({
+    required this.id,
+    required this.partner1,
+    required this.partner2,
+  });
+}
